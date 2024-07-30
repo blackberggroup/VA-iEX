77ff06e68b3696198d21cd630c68ee259e4a54d0
RyanCrux <ryan.loper@thecruxfirm.com>
2024-07-30 17:03:58 -0400

    feat: iex-shark section

---
+++ _site/assets/js/animations/vha-sharktank-animations.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Select the images
+    const image1 = document.querySelector('#shark-image-1');
+    const image2 = document.querySelector('#shark-image-2');
+
+    // Create a GSAP animation for image1
+    gsap.to(image1, {
+        y: 40,               // Move the image up by 100 pixels
+        ease: "none",          // No easing, linear animation
+        scrollTrigger: {
+            trigger: ".iex-shark__image", // The element that triggers the animation
+            start: "top 100%",  // Start the animation when the top of the trigger element reaches 90% of the viewport height
+            end: "bottom top", // End the animation when the bottom of the trigger element reaches the top of the viewport
+            scrub: true,       // Smoothly animate the progress based on the scroll position
+            //markers: true      // Show markers for the start and end positions (useful for debugging)
+        }
+    });
+
+    // Create a GSAP animation for image2
+    gsap.to(image2, {
+        y: -60,               // Move the image up by 150 pixels
+        ease: "none",          // No easing, linear animation
+        scrollTrigger: {
+            trigger: ".iex-shark__image", // The element that triggers the animation
+            start: "top 100%",  // Start the animation when the top of the trigger element reaches 80% of the viewport height
+            end: "bottom top", // End the animation when the bottom of the trigger element reaches the top of the viewport
+            scrub: true,       // Smoothly animate the progress based on the scroll position
+            //markers: true      // Show markers for the start and end positions (useful for debugging)
+        }
+    });
+});
